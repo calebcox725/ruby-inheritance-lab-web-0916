f7771b946233301de551f5cbe8f38ab60a89c8ed
calebcox725 <caleb.cox@flatironschool.com>
2016-10-01 10:43:40 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+class Student < User
+  attr_reader :knowledge
 
+  def initialize
+    self.knowledge = []
+  end
+
+  def learn(knowledge)
+    self.knowledge << knowledge
+  end
+
+  private
+
+  def knowledge=(knowledge)
+    @knowledge = knowledge
+  end
 end
